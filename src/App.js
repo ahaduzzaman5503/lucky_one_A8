00a133ae45b3e06fe9018339826cb73344b38a21
ahaduzzaman550 <ahaduzzaman5503@gmail.com>
2022-03-26 00:00:28 +0600

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './component/Header/Header'
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <h1>I am clean</h1>
     </div>
   );
---
+++ src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1 className='book-title'>Favorite books buy</h1>
+            <h2>Choose 5 books</h2>
+        </div>
+    );
+};
+
+export default Header;
